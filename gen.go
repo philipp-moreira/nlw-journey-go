cc4900797dd3c1ddc17e8655528f165ff7dc62db
Philipp Moreira <psmdeveloper@gmail.com>
2024-07-12 20:30:06 -0300

    feat add confirm participant
    class#2

--- gen.go
+++ gen.go
@@ -1,3 +1,5 @@
 package main
 
 // go:generate migrate --migrations ./internal/pgstore/migrations --config ./internal/pgstore/migrations/tern.conf
+
+// go:generate sqlc generate -f ./internal/pgstore/sqlc.yaml
